1faa529f90f41cc5481c096c1a939637a611aace
Andreas Lind <andreaslindpetersen@gmail.com>
2019-06-23 16:23:58 +0200

    Replace browserify with rollup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    require('rollup-plugin-commonjs')(),
+    require('rollup-plugin-node-resolve')(),
+    require('rollup-plugin-node-globals')(),
+    require('rollup-plugin-json')(),
+    require('rollup-plugin-terser').terser({
+      output: {
+        comments(node, comment) {
+          return /^!|@preserve|@license|@cc_on/i.test(comment.value);
+        }
+      }
+    })
+  ]
+};
